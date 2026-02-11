6b32a23c0bac421096531bfabf4c1501cdac0ccd
mishaiomdin <misha.iomdin@gmail.com>
2026-02-11 11:22:26 +0100

    add input word form

---
+++ script/wordinput.js
@@ -0,0 +1,7 @@
+
+inputWordSubmitHandler = function (event) {
+    event.preventDefault();
+    const inputWord = document.getElementById("inputWord").value;
+    console.log("Input word submitted:", inputWord);
+    //runAcceptor(inputWord);
+}
