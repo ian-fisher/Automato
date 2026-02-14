1cd6c68ea02e2c4f206a3d1fcfa17a3517e5b5d2
hhgInfoLK <hqwgbhdnhp@privaterelay.appleid.com>
2026-02-14 18:39:33 +0100

    Big changes to website. homepage, acceptor.html and aboutUs.html (README.md basicaly) connected. Added some css

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ base_dir = os.path.dirname(__file__)
 
 @app.route("/")
 def index():
-    return send_from_directory(base_dir, "index.html")
+    return send_from_directory(base_dir, "pages/homepage.html")
 
 
 @app.route("/<path:filename>")
