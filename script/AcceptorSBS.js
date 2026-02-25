4f6b860a82056cc476c72a719c9c809dcaa6b2fe
hhgInfoLK <hqwgbhdnhp@privaterelay.appleid.com>
2026-02-25 11:03:05 +0100

    multiinput for acceptorSBS

--- script/AcceptorSBS.js
+++ script/AcceptorSBS.js
@@ -33,7 +33,10 @@ var startSBSSimulation = function(word) {
 
 var nextNodeSBS = function() {
     const symbolInPosition = wordSBS[positionSBS]
-    const matchingTransition = ACCEPTOR_sbs.transitions.find((f) => f.from === currentStateSBS && f.symbol === symbolInPosition);
+    const matchingTransition = ACCEPTOR_sbs.transitions.find(
+        (f) => f.from === currentStateSBS && f.symbols.includes(symbolInPosition)
+    );
+
     if (!matchingTransition) {
         return {"rejected" : false, "exception" : "transition at state " + currentStateSBS + " is missing for " + " symbol " + symbolInPosition + "."};
     }
